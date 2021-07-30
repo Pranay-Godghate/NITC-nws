52f452c9974c98b675e9f249607b789e76ed4c22
Pranay godghate <pranay_b190560me@nitc.ac.in>
2021-07-30 15:35:04 +0530

    FLSKR

---
+++ flaskr/1308create.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS imp_user;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS tags_post;
+
+CREATE TABLE imp_user (
+  id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE post (
+  id SERIAL PRIMARY KEY,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES imp_user (id)
+);
+
+CREATE TABLE tag (
+       id SERIAL PRIMARY KEY,
+       name TEXT UNIQUE NOT NULL
+              ); 
+       
+CREATE TABLE tags_post (
+       notes INTEGER,
+       tag INTEGER,
+       FOREIGN KEY (notes) references post(id) ON DELETE CASCADE,
+       FOREIGN KEY (tag) references tag(id) ON DELETE CASCADE
+);
+
+       
+
