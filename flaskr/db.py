c1db136e7eea192020092b4be38dc109e62311c5
Pranay godghate <pranay_b190560me@nitc.ac.in>
2021-07-31 19:58:24 +0530

    change in database

--- flaskr/db.py
+++ flaskr/db.py
@@ -9,8 +9,8 @@ from flask.cli import with_appcontext
 def get_db():
     if 'db' not in g:
        if 'db' not in g:        
-          dbname = current_app.config['DATABASE'] 
-          g.db = psycopg2.connect(f"dbname={dbname}")
+          DATABASE_URL=os.environ['DATABASE']
+          g.db = psycopg2.connect(DATABASE_URL,sslmode='require')
     return g.db
 
 
